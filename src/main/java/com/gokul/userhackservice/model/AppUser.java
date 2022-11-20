50ab7131e094dd2b44b5b52fb8b7ff1cc5df06b1
gokintosh <gokul.kannambally_balachandran@student.po.edu.pl>
2022-11-20 02:30:41 +0100

    add leaderboard algorithm

--- src/main/java/com/gokul/userhackservice/model/AppUser.java
+++ src/main/java/com/gokul/userhackservice/model/AppUser.java
@@ -3,13 +3,16 @@ package com.gokul.userhackservice.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -18,8 +21,10 @@ import java.util.Set;
         @UniqueConstraint(columnNames = "username"), @UniqueConstraint(columnNames = "email")
         }
 )
-@Data
+
 @NoArgsConstructor
+@Getter
+@Setter
 public class AppUser {
 
 //        id username email password roles
@@ -64,4 +69,17 @@ public class AppUser {
         )
         private Set<Role> roles=new HashSet<>();
 
+
+        @Override
+        public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+                AppUser appUser = (AppUser) o;
+                return Objects.equals(id, appUser.id) && Objects.equals(username, appUser.username) && Objects.equals(gmnia, appUser.gmnia) && Objects.equals(address, appUser.address) && Objects.equals(deviceId, appUser.deviceId) && level == appUser.level  && Objects.equals(email, appUser.email) && Objects.equals(password, appUser.password) && Objects.equals(roles, appUser.roles);
+        }
+
+        @Override
+        public int hashCode() {
+                return Objects.hash(id, username, gmnia, address, deviceId, level, email, password, roles);
+        }
 }
